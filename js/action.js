fb19ed8cd670211d52b5231e54139a6eddb3f1df
Ryan Pecor <rpecor14@gmail.com>
2017-01-27 11:47:35 -0500

    Lights and Action
    Added lighting to make the box and scene look better. Added key
    controls to move the box along the x, y and z axes. Buttons used -
    W,S,A,D, up arrow, down arrow.

--- js/action.js
+++ js/action.js
@@ -1,11 +1,34 @@
-
+// key function to control cube position
 function Action() {
     self = this;
 
-    self.isAnyKeyDown = false;
+    self.leftPressed = false;
+    self.rightPressed = false;
+    self.upPressed = false;
+    self.downPressed = false;
 
     function handleKeyEvent(e, isKeyDown) {
-        self.isAnyKeyDown = isKeyDown
+        if(e.keyCode == 65) {
+            self.leftPressed = isKeyDown;
+        }
+
+        if(e.keyCode == 68) {
+            self.rightPressed = isKeyDown;
+        }
+
+        if(e.keyCode == 87) {
+            self.upPressed = isKeyDown;
+        }
+
+        if(e.keyCode == 83) {
+            self.downPressed = isKeyDown;
+        }
+        if(e.keyCode == 38) {
+            self.forwardPressed = isKeyDown;
+        }
+        if(e.keyCode == 40) {
+            self.backwardPressed = isKeyDown;
+        }
     }
 
     document.addEventListener("keydown", function(e) {handleKeyEvent(e, true)});
--- js/main.js
+++ js/main.js
@@ -5,12 +5,20 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeig
 var action = new Action();
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0x659EC7);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-        
+
+//Adding two lights 
+var light = new THREE.AmbientLight( 0xffffff, 0.5 );
+scene.add(light);  
+
+var light2 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light2); 
+
 // creating materials for the cube
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x0f00ff } );
+var material = new THREE.MeshLambertMaterial( { color: 0xF3FFE2 } );
         
 // cube object is made of geometry and material
 var cube = new THREE.Mesh( geometry, material );
@@ -18,15 +26,29 @@ scene.add( cube );
 
 camera.position.z = 5;
 
+//rendering the cube movement from key presses
 var render = function () {
     requestAnimationFrame( render );
 
-    if (action.isAnyKeyDown) {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.04;
+    if (action.leftPressed) {
+        cube.position.x -= 0.1;
     }
-
+    if (action.rightPressed) {
+        cube.position.x += 0.1;
+    }
+    if (action.upPressed) {
+        cube.position.y += 0.1;
+    }
+    if (action.downPressed) {
+        cube.position.y -= 0.1;
+    }
+    if (action.forwardPressed) {
+        cube.position.z -= 0.1;
+    }
+    if (action.backwardPressed) {
+        cube.position.z += 0.1;
+    }
         renderer.render(scene, camera);
     };
 
-render();
+requestAnimationFrame(render);
